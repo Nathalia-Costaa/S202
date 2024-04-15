b521c5b96e9a04b0a06e4399d9479106b507d128
Nathália Aparecida <nathaliaaparecida1804@gmail.com>
2024-04-15 01:10:07 -0300

    fix:

--- BD_1Avaliacao/MotoristaDAO.py
+++ BD_1Avaliacao/MotoristaDAO.py
@@ -8,7 +8,7 @@ class MotoristaDAO:
     def __init__(self, database):
         self.db = database
 
-    def create_motorista_DAO(self, motorista: Motorista, quant):
+    def create_motorista_DAO(self, motorista: Motorista):
         try:
             motorista_doc = {
                 "nota_motorista": int(motorista.nota_motorista),
@@ -27,32 +27,47 @@ class MotoristaDAO:
             print(f"Motorista created with id: {res.inserted_id}")
             return res.inserted_id
         except Exception as e:
-            print(f"An error occurred while creating person: {e}")
+            print(f"An error occurred while creating motorista: {e}")
             return None
 
     def read_motorista_by_id(self, id: str):
         try:
             res = self.db.collection.find_one({"_id": ObjectId(id)})
-            print(f"Person found: {res}")
+            print(f"motorista found: {res}")
             return res
         except Exception as e:
-            print(f"An error occurred while reading person: {e}")
+            print(f"An error occurred while reading motorista: {e}")
             return None
 
-    def update_motorista(self, id: str, motorista: list, nota: int):
+    def update_motorista(self, id: str, motorista: list):
         try:
-            res = self.db.collection.update_one({"_id": ObjectId(id)}, {"$set": {"motorista": motorista, "nota": nota}})
-            print(f"Person updated: {res.modified_count} document(s) modified")
+            motorista_doc = {
+                "$set": {
+                    "nota_motorista": int(motorista.nota_motorista),
+                    "corridas": [{
+                        "nota": int(corrida.nota_corrida),
+                        "distancia": float(corrida.distancia),
+                        "valor": float(corrida.valor),
+                        "passageiro": {
+                            "nome": corrida.passageiro.nome,
+                            "documento": corrida.passageiro.documento
+                        }
+                    } for corrida in motorista.corrida]
+                }
+            }
+
+            res = self.db.collection.update_one({"_id": ObjectId(id)}, motorista_doc)
+            print(f"motorista updated: {res.modified_count} document(s) modified")
             return res.modified_count
         except Exception as e:
-            print(f"An error occurred while updating person: {e}")
+            print(f"An error occurred while updating motorista: {e}")
             return None
 
     def delete_motorista(self, id: str):
         try:
             res = self.db.collection.delete_one({"_id": ObjectId(id)})
-            print(f"Person deleted: {res.deleted_count} document(s) deleted")
+            print(f"motorista deleted: {res.deleted_count} document(s) deleted")
             return res.deleted_count
         except Exception as e:
-            print(f"An error occurred while deleting person: {e}")
+            print(f"An error occurred while deleting motorista: {e}")
             return None
